c172507499346cbc7938a3faeacc648b862287c0
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-05 22:30:48 -0500

    Migrate files to new project using IdentityFramework

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        // Get: Customers/Details
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer() { Name = "Anthony Parrett", Id = 1},
+                new Customer() { Name = "Meagan Hicks", Id = 2}
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // Get: Movies/Details
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie() {Name = "The Illusionist", Id = 1},
+                new Movie() {Name = "Captain America: Civil War", Id = 2},
+                new Movie() {Name = "Back to the Future", Id = 3}
+            };
+        }
+    }
+}
