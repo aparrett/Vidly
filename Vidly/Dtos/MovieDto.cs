9d9b4f4a83702f11f174bb18ea3f9a23e423ed06
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-12 20:11:30 -0500

    Create movies API

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        public byte NumberInStock { get; set; }
+    }
+}
