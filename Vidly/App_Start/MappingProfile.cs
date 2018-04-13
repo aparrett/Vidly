b3d70562e6c01dcefee4f7650da5c07e2e73ea57
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-12 20:11:10 -0500

    Fix MappingProfile - Prevent from Mapping Ids

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -8,10 +8,17 @@ namespace Vidly
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>()
+            Mapper.CreateMap<Customer, CustomerDto>();
+
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+
+            Mapper.CreateMap<Movie, MovieDto>();
+
+            Mapper.CreateMap<MovieDto, Movie>()
                 .ForMember(m => m.Id, opt => opt.Ignore());
 
-            Mapper.CreateMap<CustomerDto, Customer>();
         }
     }
 }
