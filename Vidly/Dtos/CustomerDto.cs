e53b1e904edcf54eaece4f7979a37fa24863a356
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-12 19:21:50 -0500

    Create setup for AutoMapping

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public int MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthday { get; set; }
+    }
+}
