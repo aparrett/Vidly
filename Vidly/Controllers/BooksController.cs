ab903d03c47a3d76666f9dc546e567a2358f96a4
Anthony Parrett <anthonyparrett7@gmail.com>
2018-04-13 22:43:03 -0500

    Add Books for practice

---
+++ Vidly/Controllers/BooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class BooksController : Controller
+    {
+        public ApplicationDbContext _context;
+
+        public BooksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            return View(books);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var book = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            if (book == null)
+                return HttpNotFound();
+
+            return View(book);
+        }
+    }
+}
---
+++ Vidly/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Vidly.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Pages { get; set; }
+    }
+}
